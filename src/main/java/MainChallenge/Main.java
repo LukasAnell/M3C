cfbd2c80841815909371640ac05d44d620e8b2f3
LukasAnell <lukasmanell@gmail.com>
2024-03-03 20:08:10 -0800

    finalized comments

--- src/main/java/MainChallenge/Main.java
+++ src/main/java/MainChallenge/Main.java
@@ -21,63 +21,12 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // graphPopulationGrowth();
-        matrixFuckery();
-
-//        Guess guess = new Guess(new double[] {1, 1, 1, 1});
-//        System.out.println(guess);
-//        for(int i = 0; i < 2; i++) {
-//            gaussNewton(time, vacantHousing, guess);
-//        }
-//        System.out.println(guess);
+        graphingAndValues();
     }
 
-    public static void gaussNewton(double[] independent, double[] dependent, Guess guess) {
-        double[][] jacobianMatrix = new double[independent.length][4];
-
-
-        for(int c = 0; c < jacobianMatrix[0].length; c++) {
-            for(int r = 0; r < jacobianMatrix.length; r++) {
-                switch(c) {
-                    case 0:
-                        jacobianMatrix[r][c] = guess.evaluateDerivativeOne(independent[r]);
-                        break;
-                    case 1:
-                        jacobianMatrix[r][c] = guess.evaluateDerivativeTwo(independent[r]);
-                        break;
-                    case 2:
-                        jacobianMatrix[r][c] = guess.evaluateDerivativeThree(independent[r]);
-                        break;
-                    case 3:
-                        jacobianMatrix[r][c] = guess.evaluateDerivativeFour(independent[r]);
-                        break;
-                }
-            }
-        }
-
-        double[] residualArray = new double[dependent.length];
-        for(int i = 0; i < residualArray.length; i++) {
-            residualArray[i] = dependent[i] - guess.evaluateActual(independent[i]);
-        }
-        Matrix residualMatrix = new Matrix(new double[][] {residualArray});
-        residualMatrix = residualMatrix.transpose();
-        Matrix result = getPseudoInverse(jacobianMatrix);
-        System.out.println("result " + result.getRowDimension() + " " + result.getRowDimension());
-
-
-        Matrix parameterMatrix = new Matrix(new double[][] {guess.parameters});
-        parameterMatrix = parameterMatrix.transpose();
-        System.out.println("result: " + result.getRowDimension() + " " + result.getColumnDimension());
-        System.out.println("residual: " + residualMatrix.getRowDimension() + " " + residualMatrix.getColumnDimension());
-        result = parameterMatrix.minus(result.times(residualMatrix));
-        guess.parameters = result.getColumnPackedCopy();
-    }
-
-    public static void matrixFuckery() throws IOException {
+    public static void graphingAndValues() throws IOException {
+        // initialize vectors for time (zero through desired power of x)
         double[] timeZero = new double[] {
-                1,
-                1,
-                1,
                 1,
                 1,
                 1,
@@ -112,198 +61,140 @@ public class Main {
                 15
         };
 
-        double[] timeSquared = new double[] {
-                0,
-                1,
-                4,
-                9,
-                16,
-                25,
-                36,
-                49,
-                64,
-                81,
-                100,
-                121,
-                144,
-                169,
-                196,
-                225
+        // initialize matrix containing independent variables
+        double[][] independents = new double[][] {
+                {
+                    // median household income
+                        60665,
+                        61856,
+                        63470,
+                        65277,
+                        67365,
+                        70594,
+                        74458,
+                        79565,
+                        85562,
+                        92263,
+                        97185,
+                        105391,
+                        116068
+                },
+                {
+                    // housing prices
+                        321662,
+                        290240,
+                        287331,
+                        254806,
+                        256336,
+                        288632,
+                        302913,
+                        329175,
+                        356518,
+                        396982,
+                        435367,
+                        453289,
+                        506074,
+                },
+                {
+                    // vacant housing
+                        12604,
+                        12416,
+                        12149,
+                        11890,
+                        11717,
+                        11060,
+                        10672,
+                        11388,
+                        12057,
+                        12963,
+                        13002,
+                        14571,
+                        15568
+                },
+
+                timeZero
         };
 
-        double[] timeCubed = new double[] {
-                0,
-                1,
-                8,
-                27,
-                64,
-                125,
-                216,
-                343,
-                512,
-                729,
-                1000,
-                1331,
-                1728,
-                2197,
-                2744,
-                3375
-        };
+        // convert matrix containing time vectors to a Matrix object
+        // and initialize transposedTimeMatrix to its transpose
+        /*
+        * The lines are reversed due to the nature of initializing 2D Arrays,
+        * which would represent the vector as its transpose by default,
+        * rather than the intended matrix
+        */
+        Matrix transposedIndependentsMatrix = new Matrix(independents);
+        Matrix independentsMatrix = transposedIndependentsMatrix.transpose();
 
-        double[] timeQuartic = new double[] {
-                0,
-                1,
-                16,
-                81,
-                256,
-                625,
-                1296,
-                2401,
-                4096,
-                6561,
-                10000,
-                14641,
-                20736,
-                28561,
-                38416,
-                50625
-        };
+        // multiplies the two previous matrices, will be used in SVD
+        Matrix multMatrix = transposedIndependentsMatrix.times(independentsMatrix);
 
-        double[] timeQuintic = new double[] {
-                0,
-                1,
-                32,
-                243,
-                1024,
-                3125,
-                7776,
-                16807,
-                32768,
-                59049,
-                100000,
-                161051,
-                248832,
-                371293,
-                537824,
-                759375
-        };
 
-        double[] timeHexic = new double[] {
-                0,
-                1,
-                64,
-                729,
-                4096,
-                15625,
-                46656,
-                117649,
-                262144,
-                531441,
-                1000000,
-                1771561,
-                2985984,
-                4826809,
-                7529536,
-                11390625
-        };
+        // SVD is performed on the transposedTimeXTime
+        SingularValueDecomposition svd = new SingularValueDecomposition(multMatrix);
 
-        double[][] timeLongs = new double[][] {
-                timeZero,
-                time,
-                timeSquared,
-                timeCubed,
-                timeQuartic,
-                timeQuintic,
-                timeHexic
-        };
-
-//        double[] totalHousingUnits = new double[] {
-//                302465,
-//                304164,
-//                306694,
-//                309205,
-//                311286,
-//                315950,
-//                322795,
-//                334739,
-//                344503,
-//                354475,
-//                367337,
-//                362809,
-//                372436
-//        };
-//
-//        double[] occupiedUnits = new double[] {
-//                280453,
-//                282480,
-//                285476,
-//                288439,
-//                290822,
-//                296633,
-//                304157,
-//                314850,
-//                323446,
-//                331836,
-//                344629,
-//                337361,
-//                345246
-//
-//        };
-
-        Matrix transposedMatrix = new Matrix(timeLongs);
-        Matrix UnTransposedMatrix = transposedMatrix.transpose();
-
-        Matrix transposedTimesUnTransposed = transposedMatrix.times(UnTransposedMatrix);
-
-        SingularValueDecomposition svd = new SingularValueDecomposition(transposedTimesUnTransposed);
+        // get U and V matrices
+        // and their transposed counterparts
         Matrix UMatrix = svd.getU();
+        //U is orthogonal, so its inverse is its transpose
         Matrix transposedUMatrix = UMatrix.transpose();
         Matrix VMatrix = svd.getV();
+        //this is also the case with V
         Matrix transposedVMatrix = VMatrix.transpose();
+
+        // get vector containing singular values of multMatrix and find the recipricoals for each
+        // and create Matrix object for it
+        //In total, it is (A^T * A)^-1 * A^T
+        //(A^T * A)^-1 = V * S^-1 U^T
         double[] svds = svd.getSingularValues();
         Matrix svdMatrix = createSVDArray(svds, transposedVMatrix.getColumnDimension(), transposedUMatrix.getRowDimension());
-        Matrix inverse = VMatrix.times(svdMatrix);
-        inverse = inverse.times(transposedUMatrix);
-        Matrix transposeInverse = inverse.transpose();
 
-//        double[] occupationPercentage = new double[totalHousingUnits.length];
-//
-//        for(int i = 0; i < totalHousingUnits.length; i++) {
-//            occupationPercentage[i] = occupiedUnits[i] / totalHousingUnits[i];
-//        }
-        double[] vacantUnits = new double[] {
-                1276,
-                1276,
-                2002,
-                2002,
-                1639,
-                1431,
-                1171,
-                1254,
-                1287,
-                1222,
-                1318,
-                1340,
-                1524,
-                1586,
-                1567,
-                1277
+        // create PseudoInverse by multiplying the parts together
+        Matrix pseudoInverse = VMatrix.times(svdMatrix).times(transposedUMatrix);
+
+        // vector that stores the dependent values
+        double[] dependent = new double[] {
+                7902,
+                8501,
+                8952,
+                9022,
+                8972,
+                8899,
+                9106,
+                8949,
+                10122,
+                10730,
+                11643,
+                12112,
+                11199
         };
 
-        Matrix housingMatrix = new Matrix(new double[][] {
-                vacantUnits
+        // constructs a Matrix of the dependent values and takes the transpose
+        Matrix dependentMatrix = new Matrix(new double[][] {
+                dependent
         }).transpose();
 
-        Matrix weights = inverse.times(transposedMatrix).times(housingMatrix).transpose();
+        // computes the weights Matrix by multiplying
+        // the pseudoInverse by the transposedIndependentsMatrix
+        // then by the dependentMatrix, and then taking the transpose
+        // weights = Independents^-1 * Dependents
+        Matrix weights = pseudoInverse.times(transposedIndependentsMatrix).times(dependentMatrix).transpose();
 
+        // convert weights Matrix to a vector and print them out
         double[] weightArray = weights.getColumnPackedCopy();
         for(int i = 0; i < weightArray.length; i++) {
             System.out.println(weightArray[i]);
         }
 
+        // initialize XYSeriesCollection
         XYSeriesCollection data = new XYSeriesCollection();
 
+        // initialize new XYSeries that will store dependent vs independent relationship
         XYSeries regressionEstimate = new XYSeries("Housing Supply vs. Time");
+
+        // loops through given range of x values
+        // and computes the y values at a given point
+        // then adds ordered pair to XYSeries object
+        // this also prints out estimates at 10 years, 20 years, and 50 years
         double x = 0;
         System.out.println();
         while(x <= 50) {
@@ -315,14 +206,18 @@ public class Main {
             x++;
         }
 
+       // plots given data points as a scatterplot on the same graph as the line
         XYSeries givenDataPoints = new XYSeries("Given Points");
-        for(int i = 0; i < vacantUnits.length; i++) {
-            givenDataPoints.add(i, vacantUnits[i]);
+        for(int i = 0; i < dependent.length; i++) {
+            givenDataPoints.add(i, dependent[i]);
         }
 
+        // adds XYSeries objects to seriesCollection object
         data.addSeries(regressionEstimate);
         data.addSeries(givenDataPoints);
 
+        // initializes lineChart with given title
+        // and seriesCollection object as data source
         JFreeChart lineChart = ChartFactory.createXYLineChart(
                 "Homelessness vs. Time",
                 "Time", "Vacant Units",
@@ -331,105 +226,56 @@ public class Main {
                 false, false, false
         );
 
+        // changes scale of yAxss to fit all data points
         NumberAxis yAxis = new NumberAxis();
-        yAxis.setTickUnit(new NumberTickUnit(100));
-        yAxis.setRange(0, 3000);
+        yAxis.setTickUnit(new NumberTickUnit(5000));
+        yAxis.setRange(0, 100000);
 
-        NumberAxis xAxis = new NumberAxis();
-        xAxis.setTickUnit(new NumberTickUnit(1));
+        // initializes plot with domain and range grid hidden
+        // and rangeAxis set to pre-determined yAxis
         XYPlot plot = (XYPlot) lineChart.getPlot();
         plot.setDomainGridlinesVisible(false);
         plot.setRangeGridlinesVisible(false);
         plot.setRangeAxis(yAxis);
+        // initializes renderer and makes it so:
+        // line graph lines are visible but data points are hidden
+        // and
+        // scatter plot lines are hidden but data points are visible
         XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) plot.getRenderer();
         renderer.setSeriesLinesVisible(0, true);
         renderer.setSeriesLinesVisible(1, false);
         renderer.setSeriesShapesVisible(0, false);
         renderer.setSeriesShapesVisible(1, true);
+
+        // saves chart as an image
         File f = new File("src/main/Images/housingSupplyVsTime.png");
         ChartUtils.saveChartAsPNG(f, lineChart, 1024, 1024);
     }
 
-
-    private static Matrix getPseudoInverse(double[][] jacobian) {
-        Matrix transposedMatrix = new Matrix(jacobian);
-        Matrix UnTransposedMatrix = transposedMatrix.transpose();
-
-        Matrix transposedTimesUnTransposed = transposedMatrix.times(UnTransposedMatrix);
-
-        SingularValueDecomposition svd = new SingularValueDecomposition(transposedTimesUnTransposed);
-        Matrix UMatrix = svd.getU();
-        Matrix transposedUMatrix = UMatrix.transpose();
-        Matrix VMatrix = svd.getV();
-        Matrix transposedVMatrix = VMatrix.transpose();
-        double[] svds = svd.getSingularValues();
-
-        Matrix svdMatrix = createSVDArray(svds, transposedVMatrix.getColumnDimension(), transposedUMatrix.getRowDimension());
-        Matrix inverse = VMatrix.times(svdMatrix);
-        inverse = inverse.times(transposedUMatrix);
-        System.out.println("inverse: " + inverse.getRowDimension() + " " + inverse.getColumnDimension());
-        inverse = inverse.times(transposedMatrix);
-        System.out.println("inverse: " + inverse.getRowDimension() + " " + inverse.getColumnDimension());
-        return inverse;
-    }
-
     public static double multiplyByWeights(double x, Matrix weights) {
+        // converts weights Matrix to vector
         double[] weightArray = weights.getColumnPackedCopy();
 
+        // initializes Polynomial object using weights as coefficient
         Polynomial polynomial = new Polynomial(weightArray);
+
+        // evaluates polynomial at given point x
         return polynomial.evaluate(x);
     }
 
     public static Matrix createSVDArray(double[] svds, int rows, int cols) {
+        // initializes new matrix of just 0's of given dimensions
         double[][] svdMatrix = new double[rows][cols];
-        for(int r = 0; r < svdMatrix.length; r++) {
-            for(int c = 0; c < svdMatrix[0].length; c++) {
-                if(r == c && r < svds.length) {
-                    svdMatrix[r][c] = 1 / svds[r];
-                }
-            }
+
+        // loops through the svdMatrix and replaces the diagonal
+        // with the reciprocals of the singular values stored in a vector
+        int min = Math.min(rows, cols);
+        for(int i = 0; i < min; i++) {
+            svdMatrix[i][i] = 1/ svds[i];
         }
+
+        // returns Matrix object storing the singular values on the diagonal
         return new Matrix(svdMatrix);
     }
 
-    public static void readSpreadsheet() {
-        SAXHandler e = XmlReader.getSAXHandler();
-        assert e != null;
-    }
-
-    public static void graphPopulationGrowth() throws IOException {
-        XYSeries series = new XYSeries("Population");
-        series.add(2010, 531403);
-        series.add(2011, 539000);
-        series.add(2012, 545083);
-        series.add(2013, 549812);
-        series.add(2014, 553576);
-        series.add(2015, 556092);
-        series.add(2016, 556859);
-        series.add(2017, 556718);
-        series.add(2018, 559202);
-        series.add(2019, 559374);
-        series.add(2020, 560447);
-        series.add(2021, 562336);
-        series.add(2022, 562551);
-
-        XYSeriesCollection data = new XYSeriesCollection(series);
-
-        JFreeChart lineChart = ChartFactory.createXYLineChart(
-                "Total Population",
-                "X", "Y",
-                data,
-                PlotOrientation.VERTICAL,
-                false, false, false
-        );
-        NumberAxis yAxis = new NumberAxis();
-        yAxis.setTickUnit(new NumberTickUnit(10000));
-        yAxis.setRange(531403, 570000);
-        XYPlot plot = (XYPlot) lineChart.getPlot();
-        plot.setDomainGridlinesVisible(false);
-        plot.setRangeGridlinesVisible(false);
-        plot.setRangeAxis(yAxis);
-        File f = new File("src/main/Images/population.png");
-        ChartUtils.saveChartAsPNG(f, lineChart, 1024, 1024);
-    }
 }
--- src/main/java/MichaelMatrixMath/Polynomial.java
+++ src/main/java/MichaelMatrixMath/Polynomial.java
@@ -2,49 +2,24 @@ package MichaelMatrixMath;
 
 //polynomial in vector form
 public class Polynomial{
+    // stores the coefficients of the polynomial object in ascending powers
     public double[] coefficients;
+
+    // constructor for a Polynomial object given an array of coefficients in ascending powers
     public Polynomial(double[] arr){
         coefficients = arr;
     }
-    //Multiply two polynomials
-    public Polynomial multiply(Polynomial other){
-        Polynomial result = new Polynomial(new double[this.coefficients.length + other.coefficients.length-1]);
-        for(int i=0;i<this.coefficients.length;i++){
-            for(int j=0;j<other.coefficients.length;j++){
-                result.coefficients[i+j]+=this.coefficients[i]*other.coefficients[j];
-            }
-        }
-        return result;
-    }
 
-    public Polynomial multiply(double scalar){
-        Polynomial result = new Polynomial(new double[this.coefficients.length]);
-        for(int i = 0; i<this.coefficients.length;i++){
-            result.coefficients[i]=scalar*this.coefficients[i];
-        }
-        return result;
-    }
-
-    //add two polynomials
-    public Polynomial add(Polynomial other){
-        Polynomial result = new Polynomial(new double[Math.max(this.coefficients.length,other.coefficients.length)]);
-        for(int i=0;i<this.coefficients.length;i++){
-            result.coefficients[i]+=this.coefficients[i];
-        }
-        for(int i=0;i<other.coefficients.length;i++){
-            result.coefficients[i]+=other.coefficients[i];
-        }
-        return result;
-    }
     //evaluate polynomial at input
     public double evaluate(double input){
         double total = 0;
         for(int i = 0; i<this.coefficients.length; i++){
-            total+=this.coefficients[i]*Math.pow(input,i);
+            total += this.coefficients[i] * Math.pow(input, i);
         }
         return total;
     }
 
+    // returns polynomial as a String
     public String toString(){
         String result = "";
         for(int i=coefficients.length-1;i>0;i--){
