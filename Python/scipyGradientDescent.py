e5f5efb05a978138e533b5ae040bb541b8348af3
Lukas Anell <lukasmanell@gmail.com>
2025-02-26 12:36:20 -0800

    Templates for plotting individual country and plotting all countries

--- Python/scipyGradientDescent.py
+++ Python/scipyGradientDescent.py
@@ -29,11 +29,11 @@ def getDataPoints(filePath: str) -> ():
     return xDataTuple, yDataTupleList
 
 
-def graphCurveFit(coefficients: [], function: callable, xValues: []):
-    # plot fitted curve
-    xData = np.linspace(min(xValues) - 1, max(xValues) + 1, 1000)
-    yData = function(xData, *coefficients)
-    plt.plot(xData, yData, label="Fitted Curve")
+def getXYValues(xData, yData, delimiter):
+    # filter out data points with a delimiter
+    xValues = [float(xData[i]) for i in range(len(xData)) if yData[i] != delimiter]
+    yValues = [float(yData[i]) for i in range(len(yData)) if yData[i] != delimiter]
+    return xValues, yValues
 
 
 def logistic(x, A=1, B=1, C=1, D=1):
@@ -48,16 +48,11 @@ def exponential(x, A=1, B=1, C=1, D=1):
     return A * np.exp(B * (x - C)) + D
 
 
-def getXYValues(xData, yData, delimiter):
-    # filter out data points with a delimiter
-    xValues = [float(xData[i]) for i in range(len(xData)) if yData[i] != delimiter]
-    yValues = [float(yData[i]) for i in range(len(yData)) if yData[i] != delimiter]
-    return xValues, yValues
-
-
-def plotData(xData: [], yData: [], yLabel: str):
-    # plot data points
-    plt.scatter(xData, yData, label=yLabel)
+def graphCurveFit(coefficients: [], function: callable, xValues: []):
+    # plot fitted curve
+    xData = np.linspace(min(xValues) - 1, max(xValues) + 1, 1000)
+    yData = function(xData, *coefficients)
+    plt.plot(xData, yData, label="Fitted Curve")
 
 
 def plotMultipleDatas(xData: [], yDatas: [()]):
@@ -66,17 +61,79 @@ def plotMultipleDatas(xData: [], yDatas: [()]):
         plt.scatter(xData, yData[1], label=yData[0])
 
 
+def plotAllCountries(xData: [], yDataList: [], fitCurve: bool, function: callable, deScalingFactor: int):
+    # change graph limits based on extremes of all data points
+    ax = plt.gca()
+    yLim = 0
+    xLim = 0
+    # plot data points for all countries
+    for yData in yDataList[1:]:
+        xValues, yValues = getXYValues(xData, yData[1], '--')
+        xMean, yMean = statistics.mean(xValues), statistics.mean(yValues)
+        yValues = [y / deScalingFactor for y in yValues]
+
+        # set graph limits
+        yLim = max(yLim, (max(yValues) + yMean) / deScalingFactor)
+        xLim = max(xLim, max(xData) + 1)
+
+        scatterData(xValues, yValues, yData[0])
+        if fitCurve:
+            initialGuesses = [1, 1, xMean, yMean]
+            bounds = (0, [np.inf, np.inf, np.inf, np.inf])
+            coefficients, _ = scipy.optimize.curve_fit(function, xValues, yValues, p0=initialGuesses, bounds=bounds, method='trf')
+            graphCurveFit(coefficients, function, xData)
+    ax.set_ylim([0, yLim])
+    ax.set_xlim([min(xData) - 1, max(xData) + 1])
+
+def scatterData(xData: [], yData: [], yLabel: str):
+    # plot data points
+    plt.scatter(xData, yData, label=yLabel)
+
+
+def plotUnitedStates(xData: [], yData: [], fitCurve: bool, function: callable, deScalingFactor: int, dataLabel: str):
+    # plot data points for United States
+    xValues, yValues = getXYValues(xData, yData, '--')
+    xMean, yMean = statistics.mean(xValues), statistics.mean(yValues)
+    yValues = [y / deScalingFactor for y in yValues]
+
+    # set graph limits
+    ax = plt.gca()
+    ax.set_ylim([0, (max(yValues) + yMean) / deScalingFactor])
+    ax.set_xlim([min(xData) - 1, max(xData) + 1])
+
+    scatterData(xValues, yValues, dataLabel)
+    if fitCurve:
+        initialGuesses = [1, 1, xMean, yMean]
+        bounds = (0, [np.inf, np.inf, np.inf, np.inf])
+        coefficients, _ = scipy.optimize.curve_fit(function, xValues, yValues, p0=initialGuesses, bounds=bounds, method='trf')
+        graphCurveFit(coefficients, function, xData)
+
+
 def main():
-    # obtain data points from excel file
+
+    # obtain data points from .xlsx file
     # xColumn is a tuple with the first element being the x-axis title and the second element being the x-axis data
     # yColumnList is a list of tuples with the first element being the y-axis title and the second element being the y-axis data
     # xValues and yValues are filtered to remove any data points with a delimiter (this indicates that there is no data for that point)
     xColumn, yColumnList = getDataPoints("TCP23_data_vetted.xlsx")
     xData = xColumn[1]
-    yDataList = yColumnList[3]
+    """
+    United States : 0
+    Europe : 1
+    France : 2
+    China : 3
+    India : 4
+    Japan : 5
+    """
+    yDataList = yColumnList[0]
     dataLabel = yDataList[0]
-    xValues, yValues = getXYValues(xData, yDataList[1], '--')
 
+    # plot all countries
+    plotAllCountries(xData, yColumnList[:3] + yColumnList[4:], False, exponential, 1)
+    # plotUnitedStates(xData, yDataList[1], True, exponential, 1, dataLabel)
+
+    """
+    xValues, yValues = getXYValues(xData, yDataList[1], '--')
     # obtain mean values of relevant data
     # will be used as to change graph scaling and initial guesses for curve fitting
     xMean, yMean = statistics.mean(xValues), statistics.mean(yValues)
@@ -86,24 +143,24 @@ def main():
 
     # set graph limits
     ax = plt.gca()
-    ax.set_ylim([0, (max(yValues) + xMean) / deScalingFactor])
+    ax.set_ylim([0, (max(yValues) + yMean) / deScalingFactor])
     ax.set_xlim([min(xData) - 1, max(xData) + 1])
 
-    # scale data if necessary
-    if deScalingFactor != 1:
-        xValues = [x / deScalingFactor for x in xValues]
-        yValues = [y / deScalingFactor for y in yValues]
+    # scale data by deScalingFactor
+    # xValues = [x - 2000 for x in xValues]
+    yValues = [y / deScalingFactor for y in yValues]
 
     # plot data points
     plotData(xValues, yValues, dataLabel)
 
     # curve fitting
-    initial_guesses = [1, 1, xMean, yMean]
+    initialGuesses = [1, 1, xMean, yMean]
     bounds = (0, [np.inf, np.inf, np.inf, np.inf])
-    coefficients, _ = scipy.optimize.curve_fit(logistic, xValues, yValues, p0=initial_guesses, bounds=bounds, method='trf')
+    coefficients, _ = scipy.optimize.curve_fit(exponential, xValues, yValues, p0=initialGuesses, bounds=bounds, method='trf')
 
     # plot fitted curve
-    graphCurveFit(coefficients, logistic, xData)
+    graphCurveFit(coefficients, exponential, xData)
+    """
 
     # display graph with data points and fitted curve
     plt.legend()
